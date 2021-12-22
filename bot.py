c278c83f96c897c60e009d512da98d9cc926bd27
TimEiger <96255727+TimEiger@users.noreply.github.com>
2021-12-22 19:07:42 +0300

    Update exit_bot

--- bot.py
+++ bot.py
@@ -1,33 +1,39 @@
+# import module(please dont change import module)
 import nextcord
 from nextcord.ext import commands
 from code import *
 from asyncio.tasks import sleep
 
-bot = commands.Bot(command_prefix="you prefix bot")
-bot.remove_command('help')
-token = "you token bot"
+bot = commands.Bot(command_prefix="you prefix bot") # prefix for bot
+bot.remove_command('help')# dont delete
+token = "you token bot" # token you bot
 
 
-@commands.has_permissions(administrator=True)
-@bot.command(aliases=["cod", "CODE", "cmd"])
-@commands.cooldown(rate=1, per=5, type=commands.BucketType.guild)
-async def code(ctx, text, *, code):
+@commands.has_permissions(administrator=True)# permission for administrator
+@bot.command(aliases=["cod", "CODE", "cmd"]) # decorator for cmd
+@commands.cooldown(rate=1, per=5, type=commands.BucketType.guild) # cooldown for command
+async def code(ctx, text, *, code): # arguments for command and name main triger
 
-  if text == send:
+  if text == send:# 1 command 
     await ctx.send(code)
-  elif text == delete:
+  elif text == delete:# 2 command
     await ctx.channel.purge(limit=2)
-  elif text == mention:
+  elif text == mention:# 3 command
     await ctx.send(f"<@{code}>")
-  elif text == sendemb:
+  elif text == sendemb:# 4 command
     await ctx.send(embed=nextcord.Embed(description=code))
-  elif text == change_name_server:
+  elif text == change_name_server:# 5 command
         send1 = await ctx.reply(f"Server name change: {code}")
         server = bot.get_guild(ctx.guild.id)
         await server.edit(name = code)
         await sleep(5)
         await send1.delete()
-  elif text == 'help':
+  elif text == exit_bot:# 6 command
+        await ctx.reply("вы захотели кикнуть себя, пока\n you they wanted to kick myself, good boy\n you kick in 5 second")
+        await sleep(5)
+        author = ctx.author
+        await author.kick(reason="Kick for cmd command")
+  elif text == 'help':# help command(triger = prefix help 1)
     message = await ctx.send(" \\ ")
     await sleep(0.1)
     await message.edit(content="|")
@@ -36,8 +42,8 @@ async def code(ctx, text, *, code):
     await sleep(0.3)
     await message.delete()
     await ctx.reply("ℹ️\n`f+code send <you text>` send message \n `f+code delete 1` delete 1 message\n `f+code mention <id member>` mention for id member\n `f+code sendemb <you text>` send text in embed\n`f+code change_name_server <New name server> Change guild name!` `\nℹ️ By felter bot CMD")
-  else:
+  else:# else command not found
     await ctx.reply("Команда не найдена\nComand not found")
 
 
-bot.run(token)
+bot.run(token)# start bot
--- code.py
+++ code.py
@@ -1,7 +1,19 @@
+
+#file list command
+
+
+# 1 arguments name command
+
+# 2 arguments triger for command
+
+# please dont change default command
+
+
 send = "send"
 delete = "delete"
 mention = "mention"
 sendemb = "sendemb"
 random = "random"
+exit_bot = "exit_bot"
 change_name_server = "change_name_server"
 ban = ["stop", "STOP", "Stop", "sTop", "stOp", "stoP"]
